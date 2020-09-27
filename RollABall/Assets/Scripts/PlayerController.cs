1e2aeabbd184689411a3443fe32a011bc466917d
nickmurp <nickmurp@iu.edu>
2020-09-27 18:22:58 -0400

    wrote OnMove function

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+      
+    }
+}
