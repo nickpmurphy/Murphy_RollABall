6bc7e27a2eaa512ad847cab5557e97c929b47291
nickmurp <nickmurp@iu.edu>
2020-09-27 18:36:54 -0400

    fixed player movement speed

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
-
+      rb = GetComponent<Rigidbody>();
     }
 
     void OnMove(InputValue movementValue)
     {
-      
+      Vector2 movementVector = movementValue.Get<Vector2>();
+
+      movementX = movementVector.x;
+      movementY = movementVector.y;
+
+    }
+
+    void FixedUpdate()
+    {
+      Vector3 movement = New Vector3(movementX, 0.0f, movementY);
+
+      rb.AddForce(movement * speed);
     }
 }
