59a49399270c212f869f40506923888f0a806d9a
nickmurp <nickmurp@iu.edu>
2020-10-05 23:29:27 -0400

    tried to make cubes rotate in circles, didnt work

---
+++ RollABall/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+  float timeCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime;
+
+        float x = 0;
+        float y = Mathf.Cos (timeCounter);
+        float z = Mathf.Sin (timeCounter);
+
+        transform.position = new Vector3(x,y,z);
+    }
+}
